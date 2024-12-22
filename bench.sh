cffc5448a9ba718e8f0c860e762015c2e6d31c84
Nicolas Derumigny <nderumigny@gmail.com>
2024-12-22 16:02:48 +0100

    fix(bench): push correct directory

--- bench.sh
+++ bench.sh
@@ -8,15 +8,17 @@ else
     NB_THREADS=$1
 fi
 
+pushd $DIR
 if [ ! -d cva6 ]; then
     git clone --depth 1 --branch zcu104/no_ethernet --recurse-submodules git@github.com:NicolasDerumigny/cva6.git
 fi
 
-cd $DIR/cva6
+pushd cva6
 git clean -xdf
 export PATH=${PATH}:/opt/xpack/riscv-none-elf-gcc/bin:/opt/Xilinx/Vivado/2024.1/bin
 export RISCV=/opt/xpack
 export CVA6_REPO_DIR=${PWD}
 make clean
-/bin/time make fpga
-cd -
+/bin/time -p make fpga
+popd > /dev/null
+popd > /dev/null
